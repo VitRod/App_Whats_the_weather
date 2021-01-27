960f93df0ea3706d2a54d40364d7aac9fbcf5e68
VitRod <ukrdao@gmail.com>
2021-01-27 17:43:06 +0200

    ajax method
    request to the  server

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+
    $("#searchInput").on("keyup", function(e){
         var cityname = e.target.value;
         const APIKey = "425f263559720f69b73a57832858bdea";
+
+      //make a request to the server
+
+      $.ajax({ 
+
+
+         
+      });
+
+
+
    });
 });
