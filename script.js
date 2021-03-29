9a3de61937cca4062e6b940fea320c1b3c41bfb6
VitRod <ukrdao@gmail.com>
2021-03-29 15:19:24 +0300

    adding card with bootstrap 5
    Adding card with Bootstrap 5.  Adding HTML code to profile in script.js file

--- script.js
+++ script.js
@@ -7,15 +7,27 @@ $(document).ready(function(){
       //make a request to the server
 
       $.ajax({ 
-            url: `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}`,
+
+         url: `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`,
  
       }).done(function(weatherdata){    // добавляем обработчик при успешном выполнении запроса
             
-         $("#profile").html(weatherdata.weather[0].description);
-        
-      });
+         $("#profile").html(`
+            <div class="container">
+            <div class="row">
+                <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Weather: ${weatherdata.weather[0].description}</h5>
+                            <p class="card-text">The Temperatur at ${cityname} is = ${weatherdata.main.temp} &#8451; and it feels like it is ${weatherdata.main.feels_like} &#8451;</p>
+                            <a href="https://www.google.com/search?q=${cityname}" class="btn btn-primary">Learn more about this place</a>
+                        </div>
+                </div>
+            </div>
+            </div>`);
+        });
+
+    })
 
 
-
-   });
-}); 
+})
